e83d41a76d15542b258c3408a5582efc96b28fd0
Preston Osoro <prestonosoro56@gmail.com>
2025-03-17 17:55:25 +0300

    feat(users): add user API endpoints
    - Implement `get_users` endpoint to retrieve all users
    - Implement `create_user` endpoint to add a new user
    - Use `UserSerializer` for request validation and response serialization

---
+++ eld_backend/users/routes/user.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from users.models.user import User
+from users.serializers.user import UserSerializer  
+
+@api_view(["GET"])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
