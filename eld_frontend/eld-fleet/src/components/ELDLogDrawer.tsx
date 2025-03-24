d1b7102d5aba3df5713c52b7dde0f37918bbddae
Preston Osoro <prestonosoro56@gmail.com>
2025-03-24 12:34:28 +0300

    chore(ui): changed title

--- eld_frontend/eld-fleet/src/components/ELDLogDrawer.tsx
+++ eld_frontend/eld-fleet/src/components/ELDLogDrawer.tsx
@@ -1,7 +1,7 @@
 const ELDLogDrawer = () => {
     return (
       <div className="p-4 bg-gray-200">
-        <h2 className="text-lg font-semibold">ELD Log</h2>
+        <h2 className="text-lg font-semibold">ELD Logs</h2>
         <p>Displays Electronic Logging Device (ELD) logs.</p>
       </div>
     );
