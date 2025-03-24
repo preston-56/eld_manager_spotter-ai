c4306fc317c998969041c0bde3335fa4b8312911
Preston Osoro <prestonosoro56@gmail.com>
2025-03-24 22:42:55 +0300

    feat: Add LoginPage component with Login form integration
    - Created a new LoginPage component to handle user login.
    - Integrated the Login component inside the LoginPage.
    - Ensured LoginPage is centered on the screen using flexbox.

---
+++ eld_frontend/eld-fleet/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import Login from '@/components/login'
+
+const LoginPage = () => {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Login />
+      </div>
+    );
+  };
+  
+  export default LoginPage;
