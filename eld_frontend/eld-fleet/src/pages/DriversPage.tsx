e94829ab22ec9602cb6714006ccafb398c2f0b14
Preston Osoro <prestonosoro56@gmail.com>
2025-03-24 12:29:01 +0300

    feat(drivers): add drivers availabilty page

---
+++ eld_frontend/eld-fleet/src/pages/DriversPage.tsx
@@ -0,0 +1,11 @@
+import DriverAvailability from "@/components/DriverAvailability"; 
+
+const DriversPage = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <DriverAvailability />
+    </div>
+  );
+};
+
+export default DriversPage;
