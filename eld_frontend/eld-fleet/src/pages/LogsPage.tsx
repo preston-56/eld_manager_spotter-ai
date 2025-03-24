7116a76e0b503449944181c9e9afc1f9ba6670c2
Preston Osoro <prestonosoro56@gmail.com>
2025-03-24 12:27:59 +0300

    feat(logs): add eld logs page component

---
+++ eld_frontend/eld-fleet/src/pages/LogsPage.tsx
@@ -0,0 +1,11 @@
+import ELDLogDrawer from "../components/ELDLogDrawer";
+
+const LogsPage: React.FC = () => {
+  return (
+    <div>
+      <ELDLogDrawer />
+    </div>
+  );
+};
+
+export default LogsPage;
