7e3f1f9ae89f2a0ade0b754f54babf60119326f3
Preston Osoro <prestonosoro56@gmail.com>
2025-03-25 09:54:07 +0300

    feat(auth): implement ProtectedRoute for secure access
    - Wrapped private routes inside `ProtectedRoute` to restrict unauthorized access
    - Ensured authentication context (`AuthProvider`) is applied across the app
    - Structured routes to include both public and protected sections
    - Sidebar and main content layout remain consistent within protected routes

--- eld_frontend/eld-fleet/src/App.tsx
+++ eld_frontend/eld-fleet/src/App.tsx
@@ -8,25 +8,47 @@ import HelpPage from "./pages/HelpPage";
 import LogsPage from "./pages/LogsPage";
 import ServicesPage from "./pages/ServicesPage";
 import DriversPage from "./pages/DriversPage";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-1 p-6 mt-16">
-          <Routes>
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/trips" element={<TripPage />} />
-            <Route path="/tracking" element={<TrackingTable />} />
-            <Route path="/drivers" element={<DriversPage />} />
-            <Route path="/map" element={<MapView />} />
-            <Route path="/logs" element={<LogsPage />} />
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/help" element={<HelpPage />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+
+          {/*  Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* Protected Routes */}
+
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <div className="flex">
+                  <Sidebar />
+                  <div className="flex-1 p-6 mt-8">
+                    <Routes>
+                      <Route path="/dashboard" element={<DashboardPage />} />
+                      <Route path="/trips" element={<TripPage />} />
+                      <Route path="/tracking" element={<TrackingTable />} />
+                      <Route path="/drivers" element={<DriversPage />} />
+                      <Route path="/map" element={<MapView />} />
+                      <Route path="/logs" element={<LogsPage />} />
+                      <Route path="/services" element={<ServicesPage />} />
+                      <Route path="/help" element={<HelpPage />} />
+                    </Routes>
+                  </div>
+                </div>
+               </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
