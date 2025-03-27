8afd42bea56cdd06d0945205f7ee88c0d56645f6
Preston Osoro <prestonosoro56@gmail.com>
2025-03-27 13:29:21 +0300

    feat(trips): integrate OpenAPI schema with trip creation endpoint

--- eld_backend/trips/routes/trip.py
+++ eld_backend/trips/routes/trip.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
 from trips.models.trip import Trip  
 from trips.serializers.trip import TripSerializer  
+from trips.schema.trips import trip_schema
 
 @api_view(["GET"])
 def get_trips(request):
@@ -11,6 +13,14 @@ def get_trips(request):
     trips = Trip.objects.all()
     serializer = TripSerializer(trips, many=True)
     return Response(serializer.data)
+@swagger_auto_schema(
+    method="post",
+    request_body=trip_schema,
+    responses={
+        201: TripSerializer(),
+        400: "Invalid data",
+    },
+)
 
 @api_view(["POST"])
 def create_trip(request):
