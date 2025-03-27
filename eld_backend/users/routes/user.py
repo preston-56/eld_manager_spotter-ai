76c46fdde9c5dbfb42ef9ed2cb0ca706bfa863c4
Preston Osoro <prestonosoro56@gmail.com>
2025-03-27 13:39:19 +0300

    feat(users): import user schema and apply Swagger documentation to routes

--- eld_backend/users/routes/user.py
+++ eld_backend/users/routes/user.py
@@ -1,14 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
 from users.models.user import User
 from users.serializers.user import UserSerializer  
-
+from users.schema.users import user_schema
 @api_view(["GET"])
 def get_users(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
+@swagger_auto_schema(
+    method="post",
+    request_body=user_schema,
+    responses={
+        201: UserSerializer(),
+        400: "Invalid data",
+    },
+)
 @api_view(["POST"])
 def create_user(request):
     serializer = UserSerializer(data=request.data)
