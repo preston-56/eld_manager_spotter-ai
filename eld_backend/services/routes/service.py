8f73d9b6e3f4d59a9a32f3899b0e0354a2343a28
Preston Osoro <prestonosoro56@gmail.com>
2025-03-27 13:15:36 +0300

    feat(services): integrate OpenAPI schema for service creation
    - Imported `service_schema` and applied `swagger_auto_schema` to `create_service` endpoint.
    - Enables Swagger UI to display request body input for service creation.
    - Improves API documentation and usability.

--- eld_backend/services/routes/service.py
+++ eld_backend/services/routes/service.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
 from services.models.service import Service
 from services.serializers.service import ServiceSerializer
+from services.schema.services import service_schema
 
 @api_view(["GET"])
 def get_services(request):
@@ -11,6 +13,15 @@ def get_services(request):
     services = Service.objects.all()
     serializer = ServiceSerializer(services, many=True)
     return Response(serializer.data)
+
+@swagger_auto_schema(
+    method="post",
+    request_body=service_schema,
+    responses={
+        201: ServiceSerializer(),
+        400: "Invalid data",
+    },
+)
 
 @api_view(["POST"])
 def create_service(request):
